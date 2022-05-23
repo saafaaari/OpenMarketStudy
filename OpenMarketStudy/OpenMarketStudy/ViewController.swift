3a036dd093bd785105d53efe8f845710a3d89968
dudu <whqtkf12@naver.com>
2022-05-23 18:19:30 +0900

    feat: 앨범, 카메라에서 이미지 가져오기

--- OpenMarketStudy/OpenMarketStudy/ViewController.swift
+++ OpenMarketStudy/OpenMarketStudy/ViewController.swift
@@ -8,17 +8,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet var myImageView: UIImageView!
+    
+    let imagePickerController = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        imagePickerController.delegate = self
+        imagePickerController.allowsEditing = true
     }
+    
     @IBAction func albumButtonTapped(_ sender: Any) {
+        imagePickerController.sourceType = .photoLibrary
+        present(imagePickerController, animated: true)
     }
     
     @IBAction func cameButtonTapped(_ sender: Any) {
+        guard UIImagePickerController.isSourceTypeAvailable(.camera) else {
+            print("카메라를 쓸 수 없습니다")
+            return
+        }
+        
+        imagePickerController.sourceType = .camera
+        present(imagePickerController, animated: true)
     }
-    
+}
+
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[UIImagePickerController.InfoKey.editedImage]
+            as? UIImage else {
+            fatalError("Error")
+        }
+        
+        myImageView.image = image
+        dismiss(animated: true, completion: nil) // 반드시 dismiss 하기.
+    }
 }
 
