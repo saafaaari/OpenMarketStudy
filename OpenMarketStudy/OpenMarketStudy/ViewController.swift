e0a933854674aa9f02e72f257c5c4623c605cb66
dudu <whqtkf12@naver.com>
2022-05-23 20:52:34 +0900

    feat: 텍스트 기본 UI

--- OpenMarketStudy/OpenMarketStudy/ViewController.swift
+++ OpenMarketStudy/OpenMarketStudy/ViewController.swift
@@ -8,17 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet var myImageView: UIImageView!
+    @IBOutlet var nameTextField: UITextField!
+    @IBOutlet var costTextField: UITextField!
+    @IBOutlet var descriptionTextField: UITextField!
+    @IBOutlet var detailTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-    @IBAction func albumButtonTapped(_ sender: Any) {
-    }
-    
-    @IBAction func cameButtonTapped(_ sender: Any) {
-    }
-    
+
 }
 
