b9a6faae03318e3ca9a48f99d489de0d9048f0a1
dudu <whqtkf12@naver.com>
2022-05-24 10:08:24 +0900

    feat: 키보드 텍스트뷰 이동

--- OpenMarketStudy/OpenMarketStudy/ViewController.swift
+++ OpenMarketStudy/OpenMarketStudy/ViewController.swift
@@ -8,15 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet var nameTextField: UITextField!
-    @IBOutlet var costTextField: UITextField!
-    @IBOutlet var descriptionTextField: UITextField!
-    @IBOutlet var detailTextView: UITextView!
     
+    @IBOutlet var myTextView: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
         // Do any additional setup after loading the view.
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
-
+    
+    
+    @objc func keyboardHide(_ sender: Notification) {
+        myTextView.contentInset.bottom = 0
+    }
+    
+    @objc func keyboardShow(_ sender: Notification) {
+        if let userInfo = sender.userInfo as NSDictionary?,
+           let keyboardframe = userInfo.value(forKey: UIResponder.keyboardFrameEndUserInfoKey) as? NSValue {
+            let keyboardHeight = keyboardframe.cgRectValue.height
+            
+            myTextView.contentInset.bottom = keyboardHeight
+        }
+    }
+    
+    
 }
 
