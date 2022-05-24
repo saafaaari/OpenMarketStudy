46532388e36732661696ccf661f92a778ef85256
dudu <whqtkf12@naver.com>
2022-05-24 10:31:11 +0900

    feat: textField 입력제한

--- OpenMarketStudy/OpenMarketStudy/ViewController.swift
+++ OpenMarketStudy/OpenMarketStudy/ViewController.swift
@@ -9,9 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet var nameField: UITextField!
+    @IBOutlet var costField: UITextField!
+    @IBOutlet var stockField: UITextField!
+    
     @IBOutlet var myTextView: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        nameField.delegate = self
+        costField.delegate = self
+        stockField.delegate = self
         
         
         // Do any additional setup after loading the view.
@@ -33,7 +41,22 @@ class ViewController: UIViewController {
             myTextView.contentInset.bottom = keyboardHeight
         }
     }
-    
-    
+}
+
+extension ViewController: UITextFieldDelegate {
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        let charcterSet = CharacterSet.decimalDigits.inverted
+        let strComponents = string.components(separatedBy: charcterSet)
+        let numberFilter = strComponents.joined(separator: "")
+
+        let currentString = textField.text! as NSString
+        let newString = currentString.replacingCharacters(in: range, with: string)
+        
+        print("string: ", string)
+        print("filter: ", numberFilter)
+        
+        return string == numberFilter
+    }
 }
 
